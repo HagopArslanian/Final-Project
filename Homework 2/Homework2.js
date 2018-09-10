7fbef4f17bb460fb29634926af2c904494541f16
HagopArslanian <43149328+HagopArslanian@users.noreply.github.com>
2018-09-11 00:17:29 +0400

    Add files via upload

---
+++ Homework 2/Homework2.js
@@ -0,0 +1,15 @@
+const constant1 =3*4;
+const constant2 =2*6;
+const constant3 =(16/2)*3;
+const name ="Hagop";
+const lastname =" Arslanians";
+const homework =" homework";
+const number =2;
+alert(name + lastname + homework + number);
+if(constant1===constant3){
+      console.log("some writing");
+}else if(constant1 + constant2===constant3){
+	console.log("yaaaaaaaaay");
+}else{
+	console.log("another writing");
+}
